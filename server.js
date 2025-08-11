cdd8e0a90578b9f0e02d26c417a96100165ed8e9
michaelasham <michaelasham1@icloud.com>
2025-08-11 15:58:11 +0300

    Fixed server.js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
 
 
-import scrapeSuperEtka from './etkaScraper.js';  // adjust if needed
-import getVehicleInfo from './etkaVehicleInfo.js';  // adjust if needed
+import './etkaScraper.js';
+import './etkaVehicleInfo.js'; // This will launch the scraper server
