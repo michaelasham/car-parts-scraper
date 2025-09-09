c6fc28e0f2997679d43636dffd9487a51fc0ddd5
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2025-09-09 14:35:18 +0300

    port fix

--- bmw-scraper/server.js
+++ bmw-scraper/server.js
@@ -84,7 +84,7 @@ app.get("/get-car-details/:vin", (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = 10001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
