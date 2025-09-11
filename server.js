db2446c9d996cf358f81ab6dcab8c295ccfb8d45
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2025-09-11 14:15:11 +0300

    port test

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // server.js
+const express = require("express");
+const proxy = require("express-http-proxy");
+
+const PORT = 10000;
 
 import "./etkaScraper.js";
 import "./etkaVehicleInfo.js"; // This will launch the scraper server
 import "./bmw-scraper/realoem.js"; // This will launch the BMW scraper server
+
+const app = express();
+// Simple health check endpoint
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Scraper services running" });
+});
+
+// Forward requests to appropriate services
+app.use("/superetka/scrape", proxy("http://localhost:3000/superetka/scrape"));
+app.use(
+  "/superetka/getVehicleInfo",
+  proxy("http://localhost:3001/superetka/getVehicleInfo")
+);
+app.use("/find-part", proxy("http://localhost:5000/find-part"));
+app.use("/get-car-details", proxy("http://localhost:5000/get-car-details"));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
