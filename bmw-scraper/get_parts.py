8671d75e63ba6b3e7e77f0787e06fd90dec91973
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2025-09-12 14:28:07 +0300

    performance optimization

--- bmw-scraper/get_parts.py
+++ bmw-scraper/get_parts.py
@@ -12,11 +12,21 @@ def main():
     part = " ".join(sys.argv[2:])  # Join all remaining args as part
 
     with Stealth().use_sync(sync_playwright()) as p:
-        browser = p.chromium.launch(headless=True,timeout=120000)
-        page = browser.new_page()
+        browser = p.chromium.launch(headless=True,timeout=30000, args=[
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ])
+        context = browser.new_context(viewport={"width": 1280, "height": 720})
+        page = context.new_page()
         page.route("**/*", block_ads)
         page.goto("http://www.realoem.com")
-        page.wait_for_load_state('networkidle')
+        page.wait_for_load_state('domcontentloaded')
         actions = Actions(page)
         result = actions.find_ac_part_by_keyword(vin, part)
         import json
