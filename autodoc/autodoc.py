953ae9fa9c85be2f8bdffc5e04087b2b1f798e00
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2025-09-24 11:59:15 +0300

    autodoc images

--- autodoc/autodoc.py
+++ autodoc/autodoc.py
@@ -10,7 +10,7 @@ def main():
         sys.exit(1)
     part_num = str(sys.argv[1]).capitalize()
     with Stealth().use_sync(sync_playwright()) as p:
-        browser = p.chromium.launch(headless=False,timeout=30000)
+        browser = p.chromium.launch(headless=True,timeout=30000)
         context = browser.new_context()
         context.set_default_timeout(60000)
         context.set_default_navigation_timeout(60000)
