d31598ea6e9d163341bfac2e4dd9c34ac19b0331
MoAshrafPT <121402445+MoAshrafPT@users.noreply.github.com>
2025-10-04 11:36:14 +0300

    made all scrapers headless

--- mercedes-scraper/get_vehicle_data.py
+++ mercedes-scraper/get_vehicle_data.py
@@ -15,7 +15,7 @@ def main():
     vin = sys.argv[1]
 
     with Stealth().use_sync(sync_playwright()) as p:
-        browser = p.chromium.launch(headless=False,timeout=30000)
+        browser = p.chromium.launch(headless=True,timeout=30000)
         context = browser.new_context()
         context.set_default_timeout(60000)
         context.set_default_navigation_timeout(60000)
